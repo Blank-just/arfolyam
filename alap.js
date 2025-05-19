6e3dcfe3bc6ca4a9ea9bde5832b1359fc706b09a
Blank-just <boglarka.karpacs@gmail.com>
2025-05-19 12:54:26 +0200

    4penznem

--- alap.js
+++ alap.js
@@ -1,3 +1,33 @@
+fetch("https://api.exchangerate-api.com/v4/latest/usd")
+.then(x=>x.json())
+.then(y=>megjelenitusd(y))
 
+function megjelenitusd(y){
+    console.log(y)
+    document.getElementById("usd").innerHTML=y.rates.HUF+" Ft"
+}
 
+fetch("https://api.exchangerate-api.com/v4/latest/eur")
+.then(x=>x.json())
+.then(y=>megjeleniteur(y))
 
+function megjeleniteur(y){
+    console.log(y)
+    document.getElementById("eur").innerHTML=y.rates.HUF+" Ft"
+}
+fetch("https://api.exchangerate-api.com/v4/latest/gbp")
+.then(x=>x.json())
+.then(y=>megjelenitgbp(y))
+
+function megjelenitgbp(y){
+    console.log(y)
+    document.getElementById("gbp").innerHTML=y.rates.HUF+" Ft"
+}
+fetch("https://api.exchangerate-api.com/v4/latest/eur")
+.then(x=>x.json())
+.then(y=>megjeleniteur(y))
+
+function megjeleniteur(y){
+    console.log(y)
+    document.getElementById("eur").innerHTML=y.rates.HUF+" Ft"
+}
