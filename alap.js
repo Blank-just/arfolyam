c153492107f61ccaa9482f94b139ca53c8e48034
Blank-just <boglarka.karpacs@gmail.com>
2025-05-19 12:58:41 +0200

    juan

--- alap.js
+++ alap.js
@@ -15,6 +15,7 @@ function megjeleniteur(y){
     console.log(y)
     document.getElementById("eur").innerHTML=y.rates.HUF+" Ft"
 }
+
 fetch("https://api.exchangerate-api.com/v4/latest/gbp")
 .then(x=>x.json())
 .then(y=>megjelenitgbp(y))
@@ -23,11 +24,21 @@ function megjelenitgbp(y){
     console.log(y)
     document.getElementById("gbp").innerHTML=y.rates.HUF+" Ft"
 }
-fetch("https://api.exchangerate-api.com/v4/latest/eur")
-.then(x=>x.json())
-.then(y=>megjeleniteur(y))
 
-function megjeleniteur(y){
+fetch("https://api.exchangerate-api.com/v4/latest/ron")
+.then(x=>x.json())
+.then(y=>megjelenitron(y))
+
+function megjelenitron(y){
     console.log(y)
-    document.getElementById("eur").innerHTML=y.rates.HUF+" Ft"
+    document.getElementById("ron").innerHTML=y.rates.HUF+" Ft"
+}
+
+fetch("https://api.exchangerate-api.com/v4/latest/cny")
+.then(x=>x.json())
+.then(y=>megjelenitcny(y))
+
+function megjelenitcny(y){
+    console.log(y)
+    document.getElementById("cny").innerHTML=y.rates.HUF+" Ft"
 }
